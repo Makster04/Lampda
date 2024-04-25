39ffad312d6ed6dcec99e90e61884416b295c33f
Mak Trnka <maktrnka@comcast.net>
2024-04-24 22:55:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,48 @@
-// import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
-const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
+import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 
-const s3Client = new S3Client({ region: 'us-west-2' });
+const s3Client = new S3Client({ region: 'us-west-2' }); 
 
-function processStream(stream) {
-  return new Promise((resolve, reject) => {
-    let data = '';
-    stream.on('data', (chunk) => {
-      data += chunk;
-    });
-    stream.on('error', (error) => {
-      console.error('SOMEHTING BAD HAPPENED WHILE PARSING STREAM', error);
-      reject(error);
-    });
-    stream.on('end', () => {
-      console.log('ALL DATA IS HERE', data);
-      resolve(data);
-    });
-  });
-}
-
-// export handler = async (event) => { //  use this syntax on AWS Lambda
-const handler = async (event) => { // use this syntax when running code locally
-  let { bucket, object } = event.Records[0].s3;
-  console.log('HERE IS MY EVENT INFO', bucket, object);
-  let getCommand = {
-    Bucket: bucket.name,
-    Key: object.key
-  }
-
-  let fileData = await s3Client.send(new GetObjectCommand(getCommand));
-  let stream = fileData.Body;
-
-  let data = await processStream(stream);
-
-  console.log('HERE IS THE FILE DATA', data);
-
-  console.log('HERE ARE MY FOLDER AND FILE :', bucket.name, object.key, object.size);
-
-
-  // to write
-  let putCommand = {
-    Bucket: bucket.name,
-    ContentType: 'application/json',
-    Body: JSON.stringify({ name: 'Jacob' }),
-    Key: 'output/name.json'
-  }
-
+export const handler = async (event) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const fileData = event.Records[0].s3.object;
+    
+  
+  let images;
   try {
-    await s3Client.send(new PutObjectCommand(putCommand));
-    console.log('WE WRITE SOMETHING!!');
-  } catch (e) {
-    console.log('SOMETHING WENT WRONG ON WRITE: ', e);
+    // Try to download the "images.json" file
+    const data = await s3Client.send(new GetObjectCommand({ Bucket: bucket, Key: 'output/images.json' }));
+    images = JSON.parse(await streamToString(data.Body));
+  } catch (error) {
+    // If the file doesn't exist, create an empty array
+    images = [];
   }
 
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('test'),
-  };
-  return response;
+  // Add the new image to the array
+  const regex = /(.*)\.(.*)$/;
+  const match = fileData.key.replace('input/', '').match(regex);
+  let name = match[1]; // The name of the file (without the extension)
+  let type = match[2]; // The extension of the file
+    
+    
+  images.push({ name: name, size: fileData.size, type: type });
+  
+  // Upload the updated file
+  await s3Client.send(new PutObjectCommand({
+    Bucket: bucket,
+    Key: 'output/images.json',
+    Body: JSON.stringify(images),
+    ContentType: 'application/json'
+  }));
+    
+  console.log(images);
 };
 
-// don't do this on AWS, will cause issues since they expect .mjs files which uses the export keyword
-module.exports = {
-  handler
+// Helper function to convert a stream to a string
+function streamToString(stream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on('data', (chunk) => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  });
 }
